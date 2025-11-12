b8ead6e3cddee422d1918d35f493f09ac1ed870a
jss <joaoseisei@gmail.com>
2025-11-12 18:54:52 -0300

    corrige hot reload

--- server/src/main.ts
+++ server/src/main.ts
@@ -3,17 +3,17 @@ import { AppModule } from './infrastructure/nestjs/module/app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
-    const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule);
 
-    const config = new DocumentBuilder()
-        .setTitle('EngNet API')
-        .setDescription('Documentação da API EngNet')
-        .setVersion('1.0')
-        .addBearerAuth()
-        .build();
+  const config = new DocumentBuilder()
+    .setTitle('EngNet API')
+    .setDescription('Documentação da API EngNet')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
 
-    const document = SwaggerModule.createDocument(app, config);
-    SwaggerModule.setup('', app, document);
-    await app.listen(process.env.PORT ?? 3000);
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('', app, document);
+  await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
